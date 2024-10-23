99ba96dd3569338f771c4f6b06f81b56a67b21a7
c-santos <jcms121899@gmail.com>
2024-10-23 21:00:33 +0800

    added seeder

---
+++ src/db/seed.ts
@@ -0,0 +1,30 @@
+import { db } from './db.config';
+import { users } from './schemas/users.schema';
+
+async function seed() {
+  console.info('Running seeders...');
+  try {
+    // Clear database
+    await db.delete(users);
+
+    // Insert results
+    const result = await db.insert(users).values([
+      {
+        id: '2f7be137-517e-49dd-b752-5b00de61e223',
+        username: 'user1',
+      },
+      {
+        id: 'b5e4e5f5-90db-4e5f-bb3f-baabb816740d',
+        username: 'user2',
+      },
+    ]);
+    console.info('Insert to users table: ', result);
+
+
+    console.info('Seed success!')
+  } catch (error) {
+    console.error('Error seeding...', error);
+  }
+}
+
+seed();
